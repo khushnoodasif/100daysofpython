0dff9cb95e2d0b4cb1c5d2da133132838fe132c0
Khushnood Asif <72355837+khushnoodasif@users.noreply.github.com>
2021-05-19 19:20:42 +0100

    Update day2-bmi-calculator.py

--- day2-bmi-calculator.py
+++ day2-bmi-calculator.py
@@ -1,5 +1,5 @@
 print("Calculate your BMI using your weight and height. \n")
 w = float(input("What is your weight in kg? \n"))
 h = float(input("what is your height in m? \n"))
-BMI = str(w/h**2)
-print("BMI: " + BMI + "\n")
+BMI = round(w/h**2)
+print(f"Your BMI is: {BMI}")
