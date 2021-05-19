17f5777c7d9867e4cc50cf35b315f9d04d80b045
Khushnood Asif <72355837+khushnoodasif@users.noreply.github.com>
2021-05-19 20:12:09 +0100

    Update day3-ride-height-verification-and-pricing.py

--- day3-ride-height-verification-and-pricing.py
+++ day3-ride-height-verification-and-pricing.py
@@ -1,13 +1,21 @@
 print("Welcome to the rollercoaster! \n")
 height = int(input("What is your height in cm? \n"))
+bill = 0
 if height >= 150: 
   print("You can ride! \n")
   age = int(input("For pricing, what is your age? \n"))
   if age < 12:
+    bill = 5
     print("Please pay $5. \n")
   elif age <= 18:
+    bill = 7
     print("Please pay $7. \n")
   else:
+    bill = 12
     print("Please pay $12. \n")
+  photo = input("Do you need a photo taken? (Yes/No) \n")
+  if photo == "Yes":
+    bill += 3
+  print(f"Your bill is: {bill}")
 else:
   print("You can not ride! \n")
