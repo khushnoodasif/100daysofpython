95b56b273066df2691f959685c2d68b270e48768
Khushnood Asif <72355837+khushnoodasif@users.noreply.github.com>
2021-05-22 00:43:28 +0100

    Create day5-highest-score-calculator.py

---
+++ day5-highest-score-calculator.py
@@ -0,0 +1,10 @@
+print("Highest Score Calculator")
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+highest_score = 0 
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+print(f"The highest scoreL {highest_score}")
